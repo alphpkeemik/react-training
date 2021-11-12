f9b01cee05f9411b38584a3f99f7200652c7ec96
Mati Andreas <mati.andreas@ambientia.ee>
2021-11-12 16:36:50 +0200

    add lazy loading components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, {lazy, Suspense} from "react";
 
 import {Route, Routes} from "react-router-dom";
-import Todos from "./Totos";
-import Register from "./Register";
-import EditTodo from "./EditTodo";
+
+const TodosRoute = lazy(() => import(/* webpackChunkName: "Todos" */ './Totos'))
+const RegisterRoute = lazy(() => import(/* webpackChunkName: "Register" */ './Register'))
+const EditTodoRoute = lazy(() => import(/* webpackChunkName: "EditTodo" */'./EditTodo'))
 
 function Router() {
     return (
-        <Routes>
-            <Route path="/" element={<Todos/>}/>
-            <Route path="/todos/:id" element={<EditTodo/>}/>
-            <Route path="/register" element={<Register/>}/>
-        </Routes>
+        <Suspense fallback={'<span>Loading</span>'}>
+            <Routes>
+                <Route path="/" element={<TodosRoute/>}/>
+                <Route path="/todos/:id" element={<EditTodoRoute/>}/>
+                <Route path="/register" element={<RegisterRoute/>}/>
+            </Routes>
+        </Suspense>
     );
 }
 
