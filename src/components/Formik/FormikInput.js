b7d20ccd996bf1685c3a06339a74ef57f31cb6d9
Mati Andreas <mati.andreas@ambientia.ee>
2021-11-12 16:10:24 +0200

    reacfor formik 2

--- src/components/Formik/FormikInput.js
+++ src/components/Formik/FormikInput.js
@@ -5,15 +5,14 @@ import Input from "../Input";
 export default function FormikInput({
                                         formik, name, label, type
                                     }) {
+    const fieldProps = formik.getFieldProps(name)
     return (
         <Input
             label={label}
-            name={name}
             type={type}
             value={formik.values[name]}
             error={formik.touched[name] && formik.errors[name]}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
+            {... fieldProps}
         />
     );
 }
