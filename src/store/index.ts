24c1e93dce00a22aaae82b003d84754b97993ea5
Mati Andreas <mati.andreas@ambientia.ee>
2022-01-21 14:47:34 +0200

    add redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,7 +4,11 @@ import todosReducer from "./todos";
 
 const rootReducer = combineReducers({todo: todosReducer})
 
-const store = createStore(rootReducer)
+const store = createStore(
+    rootReducer,
+    /* @ts-ignore */
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+)
 
 export type TRootState = ReturnType<typeof rootReducer>
 export default store;
